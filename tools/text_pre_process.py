b1c8778665241f86d70343a9296ed1366fb8c4e8
1339503169 <1339503169@qq.com>
2020-09-08 16:55:18 +0800

    Update text_pre_process.py

--- tools/text_pre_process.py
+++ tools/text_pre_process.py
@@ -1,10 +1,13 @@
 import re
 def get_ind_name(strings):
+#     识别两种合同书写方式 第一条 或者 一
     pattern_d = re.compile('(第[一二三四五六七八九十]{1,2}(部分|条))')
     pattern_t = re.compile('([一二三四五六七八九十]{1,2}[、 ])')
     partname = {}
     if re.findall(pattern_d, strings) and re.findall(pattern_t, strings):
+#         这是两种情况都存在的情况 此时 那个正则识别的多用哪个
         if len(re.findall(pattern_d, strings)) >= len(re.findall(pattern_t, strings)):
+#         找到第一条描述的字符 放进list 然后找到字符所在的位置 将位置存储 然后遍历两个list 对合同进行拆分 之后大致都是这个流程
             patter = [c[0].replace('、', '').replace(' ', '') for c in re.findall(pattern_d, strings)]
             pattern = []
             for i in patter:
